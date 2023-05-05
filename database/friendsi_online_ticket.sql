1b5851b06875b09e9b8f6eb5a2f7334eafbd2107
official-mafuzalam <everythinggood885@gmail.com>
2023-05-05 22:33:14 +0600

    new db added

--- database/friendsi_online_ticket.sql
+++ database/friendsi_online_ticket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2023 at 12:01 PM
+-- Generation Time: May 05, 2023 at 06:32 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -44,6 +44,20 @@ CREATE TABLE `sell_ticket_history` (
   `gender` varchar(30) NOT NULL,
   `insert_date_time` datetime(6) NOT NULL DEFAULT current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sell_ticket_history`
+--
+
+INSERT INTO `sell_ticket_history` (`s_no`, `ticket_id`, `coach_no`, `coach_id`, `route`, `date`, `time`, `seat`, `fare`, `total_fare`, `station`, `mobile`, `name`, `gender`, `insert_date_time`) VALUES
+(1, '6454c6388c91a', '101', '10456', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'A1', 700, 700, 'Khulna - 700', '01747503257', 'MR. Mafuz Alam', 'Male', '2023-05-05 15:02:48.575938'),
+(2, '6454ef7679f80', '101', '10456', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'A3', 500, 500, 'Gopalgonj', '01747503257', 'MR. Mafuz Alam', 'Male', '2023-05-05 17:58:46.499641'),
+(3, '6454f014522fd', '101', '10456', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'B1B2', 600, 1200, 'Fakirhat', '01747503257', 'MR. Mafuz Alam', 'Female', '2023-05-05 18:01:24.336727'),
+(4, '6454f084dbbb5', '101', '24226', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'A4', 600, 600, 'Nawapara', '01751944774', 'MR. ', '', '2023-05-05 18:03:16.900093'),
+(5, '6454f0e15ef91', '101', '24226', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'B4', 500, 500, 'Gopalgonj', '01747503257', 'MR. Mafuz Alam', '', '2023-05-05 18:04:49.389151'),
+(6, '6454f10c9f8ba', '101', '24226', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'C4', 500, 500, 'Gopalgonj', '01747503257', 'MR. Mafuz Alam', '', '2023-05-05 18:05:32.653547'),
+(7, '6454f4149a23b', '101', '10456', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'A4', 450, 450, 'Muksudpur', '01747503257', 'MR. Mafuz Alam', 'Male', '2023-05-05 18:18:28.631496'),
+(8, '64550dfcd7f17', '101', '10456', 'Dhaka - Gopalganj - Khulna', '2023-05-05', '06:30 AM', 'J5', 450, 0, 'Muksudpur', '+9656565', 'MR. mafuz', 'Female', '2023-05-05 20:09:00.884573');
 
 -- --------------------------------------------------------
 
@@ -104,6 +118,45 @@ CREATE TABLE `trip_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `trip_status`
+--
+
+INSERT INTO `trip_status` (`s_no`, `id`, `coach_no`, `status`, `date`, `time`, `route`, `station`, `A1`, `A2`, `A3`, `A4`, `B1`, `B2`, `B3`, `B4`, `C1`, `C2`, `C3`, `C4`, `D1`, `D2`, `d3`, `d4`, `e1`, `e2`, `e3`, `e4`, `f1`, `f2`, `f3`, `f4`, `g1`, `g2`, `g3`, `g4`, `h1`, `h2`, `h3`, `h4`, `i1`, `i2`, `i3`, `i4`, `j1`, `j2`, `j3`, `j4`, `j5`) VALUES
+(1, '23356', '101', 1, '2023-05-01', '06:30 AM', 'Dhaka - Gopalganj - Khulna', 'Muksudpur - 450, Gopalgonj - 500, Fakirhat - 600, Khulna -700', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(2, '5368', '180', 1, '2023-05-01', '10:30 PM', 'Dhaka - Gopalganj - Khulna', 'Muksudpur - 450, Gopalgonj - 500, Fakirhat - 600, Khulna -700', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(3, '44401', '210', 1, '2023-05-01', '06:00 AM', 'Dhaka - Gopalganj - Pirojpur', 'Muksudpur - 450, Gopalgonj - 500, Patgati - 550, Nazirpur - 600, Pirojpur -700', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(4, '41024', '295', 1, '2023-05-01', '11:00 PM', 'Dhaka - Gopalganj - Pirojpur', 'Muksudpur - 450, Gopalgonj - 500, Patgati - 550, Nazirpur - 600, Pirojpur -700', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(5, '71714', '310', 1, '2023-05-01', '06:15 AM', 'Dhaka - Gopalganj - Kotalipara', 'Muksudpur - 450, Gopalgonj - 500, Kotalipara - 550', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(6, '54471', '390', 1, '2023-05-01', '10:15 PM', 'Dhaka - Gopalganj - Kotalipara', 'Muksudpur - 450, Gopalgonj - 500, Kotalipara - 550', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(7, '11798', '410', 1, '2023-05-01', '06:45 AM', 'Dhaka - Vatiyapara - Narail', 'Vatiyapara - 450, Narail - 550', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(8, '16692', '470', 1, '2023-05-01', '09:45 PM', 'Dhaka - Vatiyapara - Narail', 'Vatiyapara - 450, Narail - 550', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(9, '10456', '101', 1, '2023-05-05', '06:30 AM', 'Dhaka - Gopalganj - Khulna', 'Muksudpur - 450, Gopalgonj - 500, Fakirhat - 600, Khulna -700', 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+(10, '24226', '101', 1, '2023-05-05', '06:30 AM', 'Dhaka - Gopalganj - Khulna', 'Muksudpur - 450, Gopalgonj - 500, Fakirhat - 550, Nawapara - 600, Khulna -700', 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `user_type` int(5) NOT NULL,
+  `counter_name` varchar(30) NOT NULL,
+  `user_name` varchar(30) NOT NULL,
+  `mobile` varchar(30) NOT NULL,
+  `password` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `user_type`, `counter_name`, `user_name`, `mobile`, `password`) VALUES
+(1, 1, 'Hemayetpur', 'Mafuz Alam', '01747503257', 123456),
+(2, 3, 'Hemayetpur', 'MD Titu Mir', '01751944774', 123456);
+
+--
 -- Indexes for dumped tables
 --
 
@@ -120,6 +173,12 @@ ALTER TABLE `trip_status`
   ADD PRIMARY KEY (`s_no`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -127,13 +186,19 @@ ALTER TABLE `trip_status`
 -- AUTO_INCREMENT for table `sell_ticket_history`
 --
 ALTER TABLE `sell_ticket_history`
-  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `trip_status`
 --
 ALTER TABLE `trip_status`
-  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `s_no` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
